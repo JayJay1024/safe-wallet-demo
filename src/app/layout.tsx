95884acde65990ea90422dbbb55c0206c3b042e8
JayJay1024 <lygm1062@gmail.com>
2024-02-19 18:50:16 +0800

    chore: manifest matadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Safe{Wallet} Demo",
   description: "Safe Wallet demo",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
