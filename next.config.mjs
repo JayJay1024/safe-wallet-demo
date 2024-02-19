77eabbb60f3538c89d5f03ae4fbf805eb47f9a94
JayJay1024 <lygm1062@gmail.com>
2024-02-19 15:42:36 +0800

    build: update config and github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/safe-wallet-demo",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
