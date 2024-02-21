b3166674dfe21815909d25daa2bb833078d9ebb8
JayJay1024 <lygm1062@gmail.com>
2024-02-21 16:35:34 +0800

    chore: custom wallets

--- src/providers/rainbow-provider.tsx
+++ src/providers/rainbow-provider.tsx
@@ -1,19 +1,27 @@
 "use client";
 
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
+import { connectorsForWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { createConfig, WagmiProvider } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, base, zora, sepolia } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
+import { metaMaskWallet, rainbowWallet, safeWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
+import { createClient, http } from "viem";
 
-const config = getDefaultConfig({
-  appName: "Safe{Wallet} Demo",
-  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || "",
-  chains: [mainnet, polygon, optimism, arbitrum, base, zora, sepolia],
-  // ssr: true,
-});
+const appName = "Safe{Wallet} Demo";
+const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || "";
 const queryClient = new QueryClient();
+
+const connectors = connectorsForWallets(
+  [{ groupName: "Recommended", wallets: [rainbowWallet, metaMaskWallet, walletConnectWallet, safeWallet] }],
+  { appName, projectId }
+);
+const config = createConfig({
+  connectors,
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora, sepolia],
+  client: ({ chain }) => createClient({ chain, transport: http() }),
+});
 
 export default function RainbowProvider({ children }: PropsWithChildren<unknown>) {
   return (
